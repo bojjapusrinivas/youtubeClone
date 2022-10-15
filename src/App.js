25c09ef5b652a0f8ce60aaa00c261f7f0cf3a7e3
B Srinivas <31658616+bojjapusrinivas@users.noreply.github.com>
2022-10-15 11:09:06 +0530

    Project Setup
    React Project setup with folders and data, Styles, Croco files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Typography } from '@mui/material';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant='h4' textAlign="center" paddingY={2} fontWeight={500} borderBottom={1}>YOUTUBE CLONE TEMPLATE</Typography>
+    </div >
+  );
+}
+
+export default App;
